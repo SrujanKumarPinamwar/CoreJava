8d9e1cabc18cdb4f80735d46ebaf31309ea2e577
SrujanKumarPinamwar <srujankumarpinamwar@gmail.com>
2016-06-08 23:00:39 -0700

    Added iterating ArrayList with enumeration

---
+++ collections/src/com/java/collections/list/ArrayListWithEnumeration.java
@@ -0,0 +1,31 @@
+package com.java.collections.list;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+
+public class ArrayListWithEnumeration {
+
+	
+	
+	public static void main(String args[]){
+		
+		ArrayList<String> arrlist = new ArrayList<String>();
+		
+		arrlist.add("C");
+		arrlist.add("C++");
+		arrlist.add("JAVA");
+		arrlist.add("SQL");
+		arrlist.add("DOTNET");
+		
+		Enumeration<String> enumeration = Collections.enumeration(arrlist);
+		
+		while(enumeration.hasMoreElements())
+			System.out.println(enumeration.nextElement());
+		
+		System.out.println("\n"+"Size: "+ arrlist.size());
+		
+	}
+	
+	
+}
